80f3ec3022ad715a667384de879074d233100f25
Rplus <rplus.tw@gmail.com>
2022-01-20 13:10:50 +0800

    task: support arg to select pull data types

--- task/pullData.mjs
+++ task/pullData.mjs
@@ -4,41 +4,50 @@ import csv from 'csvtojson';
 import csvOptions from './csvOptions.mjs';
 import { saveCSV, outputJSON, getArgs } from './u.mjs';
 
+let type = getArgs()?.type;
 
-getData('pet.json').then(d => {
-  outputJSON(d, './public/data/raw/pet.raw.json');
-  d = clearData(d);
-  outputJSON(d, './public/data/raw/pet.src.json');
-  outputJSON(d, './public/data/pet.min.json', null);
-});
-
-getData('monster.json').then(d => {
-  outputJSON(d, './public/data/raw/monster.raw.json');
-  d = clearData(d);
-  outputJSON(d, './public/data/raw/monster.src.json');
-  outputJSON(d, './public/data/monster.min.json', null);
-});
-
-getData('skill.json').then(d => {
-  outputJSON(d, './public/data/raw/skill.raw.json');
-  d = clearData(d);
-  outputJSON(d, './public/data/raw/skill.src.json');
-  outputJSON(d, './public/data/skill.min.json', null);
-});
-
-getData('item.json').then(d => {
-  outputJSON(d, './public/data/raw/item.raw.json');
-  d = clearData(d);
-  outputJSON(d, './public/data/raw/item.src.json');
-  outputJSON(d, './public/data/item.min.json', null);
-
-  var lite = d.map(i => {
-    var {id, name, zh, tier, image} = i;
-    return {id, name, zh, tier, image};
+if (type === 'pet' || !type) {
+  getData('pet.json').then(d => {
+    outputJSON(d, './public/data/raw/pet.raw.json');
+    d = clearData(d);
+    outputJSON(d, './public/data/raw/pet.src.json');
+    outputJSON(d, './public/data/pet.min.json', null);
   });
-  outputJSON(lite, './public/data/raw/item-lite.src.json');
-  outputJSON(lite, './public/data/item-lite.min.json', null);
-});
+}
+
+if (type === 'monster' || !type) {
+  getData('monster.json').then(d => {
+    outputJSON(d, './public/data/raw/monster.raw.json');
+    d = clearData(d);
+    outputJSON(d, './public/data/raw/monster.src.json');
+    outputJSON(d, './public/data/monster.min.json', null);
+  });
+}
+
+if (type === 'skill' || !type) {
+  getData('skill.json').then(d => {
+    outputJSON(d, './public/data/raw/skill.raw.json');
+    d = clearData(d);
+    outputJSON(d, './public/data/raw/skill.src.json');
+    outputJSON(d, './public/data/skill.min.json', null);
+  });
+}
+
+if (type === 'item' || !type) {
+  getData('item.json').then(d => {
+    outputJSON(d, './public/data/raw/item.raw.json');
+    d = clearData(d);
+    outputJSON(d, './public/data/raw/item.src.json');
+    outputJSON(d, './public/data/item.min.json', null);
+
+    var lite = d.map(i => {
+      var {id, name, zh, tier, image} = i;
+      return {id, name, zh, tier, image};
+    });
+    outputJSON(lite, './public/data/raw/item-lite.src.json');
+    outputJSON(lite, './public/data/item-lite.min.json', null);
+  });
+}
 
 function clearData(data) {
   const gglist = [
