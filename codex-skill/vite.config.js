c81e497bef70f5d3f5a040adc0adc60c453cad27
Rplus <rplus.tw@gmail.com>
2022-04-09 06:03:32 +0800

    feat: support rollup-plugin-visualizer

--- codex-skill/vite.config.js
+++ codex-skill/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { visualizer } from 'rollup-plugin-visualizer';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [svelte()]
+  plugins: [svelte()],
+  server: {
+    // https: true,
+  },
+  build: {
+    rollupOptions: {
+      plugins: [visualizer()],
+    },
+  },
 })
