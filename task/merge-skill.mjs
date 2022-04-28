d59dcda41cae1a0fc8b10ac8372119df7710dd21
Rplus <rplus.tw@gmail.com>
2022-04-29 02:56:35 +0800

    task: overwrite skill name & intro by offical website

--- task/merge-skill.mjs
+++ task/merge-skill.mjs
@@ -10,8 +10,11 @@ function getJson(path) {
 }
 
 guideSkills.forEach(g_skill => {
-  let os = ornaSkills.find(o_skill => o_skill.title.zh === g_skill.zh);
+  let os = ornaSkills.find(o_skill => o_skill.title.en === g_skill.name);
   if (!os) { return; }
+
+  g_skill.zh = os.title.zh || g_skill.zh;
+  g_skill.description = os.intro?.en || g_skill.description;
 
   ['causes', 'gives'].forEach(prop => {
     if (os.metas?.[prop]) {
